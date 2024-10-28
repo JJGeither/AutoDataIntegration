7a123ae55d5aada86a12ef3022aceb7b5d15bf78
opc-4 <89939147+opc-4@users.noreply.github.com>
2024-10-28 02:24:49 -0400

    Change return value of getMatchingAttributes()

--- AutoIntegration/get_matching_attributes.py
+++ AutoIntegration/get_matching_attributes.py
@@ -3,7 +3,7 @@ import my_bert_score
 
 
 # This function matches the attributes of two tables' schemas
-def getMatchingAttributes(table1, table2)
+def getMatchingAttributes(table1, table2):
     # Get schemas of each table
     cols1 = table1[0]
     cols2 = table2[0]
@@ -19,22 +19,20 @@ def getMatchingAttributes(table1, table2)
     scores = scorer.get_word_similarity(','.join(cols1), ','.join(cols2))
     
     # attributes with scores greater than this will match
-    threshold = 0.2
+    threshold = float(input("Input threshold: "))
     
     # if attribute in cols2 has high score with attribute in cols1, it will match with that attribute
-    used_cols2 = []
+    table1Map = {attr:attr for attr in cols1}
+    table2Map = {attr:attr for attr in cols2}
     for i in range(0, len(scores)):
+        highestScore = -1
         for j in range(0, len(scores[0])):
-            if scores[i][j] >= threshold:
-                print(cols1[i] + " matches with " + cols2[j])
-                used_cols2.append(cols2[j])
-    
-    # if attribute in table 2 did not have a match in table 1, it must be added to the schema
-    final_schema = cols1.copy()
-    for attr in cols2:
-        if attr not in used_cols2:
-            final_schema.append(attr)
+            if scores[i][j] >= threshold and scores[i][j] > highestScore:
+                highestScore = scores[i][j]
+                # RHS is name in final schema. Replace later with user's choice (temporarily using table 1)
+                table1Map[cols1[i]] = cols1[i]
+                table2Map[cols2[j]] = cols1[i]
+                break
     
     # return final_schema
-    print(final_schema)
-    return final_schema
+    return table1Map, table2Map
