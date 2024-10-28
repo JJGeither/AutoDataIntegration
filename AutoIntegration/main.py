590c505ae9e7cb3b62ab853fddf37fefd33a8ad0
opc-4 <89939147+opc-4@users.noreply.github.com>
2024-10-28 01:28:23 -0400

    Update main.py to work with new merge_tables function

--- AutoIntegration/main.py
+++ AutoIntegration/main.py
@@ -1,6 +1,7 @@
 # main.py
 from file_io import read_csv, read_category_csv
 from data_processing import categorize_table, merge_tables
+from merge_tables import merge_tables
 import csv
 
 # Load CSV File
@@ -20,18 +21,14 @@ master_categories = categorize_table(master_table, category_aliases)
 dependent_categories = categorize_table(dependent_table, category_aliases)
 
 # Merge the tables
-merged_data = merge_tables(master_categories, dependent_categories, len(master_table) - 1, len(dependent_table) - 1)
+#merged_data = merge_tables(master_categories, dependent_categories, len(master_table) - 1, len(dependent_table) - 1)
+merged_table = merge_tables(master_table, dependent_table)
 
 # Write to CSV
 with open('output.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
 
-    column_keys = list(merged_data.keys())
-    writer.writerow(column_keys)
-
-    max_length = max(len(merged_data[key].value) for key in column_keys)
-
-    for i in range(max_length):
-        row = [merged_data[key].value[i] if i < len(merged_data[key].value) else 'NULL' for key in column_keys]
+    for row in merged_table:
         writer.writerow(row)
+
     print("Successfully printed to csv file")
